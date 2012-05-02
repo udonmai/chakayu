2d4c5cd39965e09cd8a61540056f08e933630352
udonmai <udonmai@gmail.com>
2012-05-02 15:48:26 +0800

    initial

---
+++ application/controllers/room.php
@@ -0,0 +1,39 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Room extends CI_Controller {
+
+	/**
+	 * The room controller which manages the room-relative action.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 */
+	public function index()
+	{
+		$this->load->view('udonmai wa sugoi~');
+	}
+
+	public function creat()
+	{
+		
+	}
+
+	public function destroy()
+	{
+		
+	}
+
+	public function join()
+	{
+	
+	}
+
+	public function leave()
+	{
+	
+	}
+
+}
+
+/* End of file room.php */
+/* Location: ./application/controllers/room.php */
