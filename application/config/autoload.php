8fce16024bdc95db1f9d3370a48b9f39eeaadae6
udonmai <udonmai@gmail.com>
2012-05-03 20:56:10 +0800

    add static

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('twig', 'doctrine');
+$autoload['libraries'] = array('twig');
 
 
 /*
